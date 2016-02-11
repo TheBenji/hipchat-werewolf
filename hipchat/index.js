82efcf044c1a4c67160fd21198b2201115ee945f
Szczygiel, Jakub <Jakub.Szczygiel@tuitravel.com>
2016-02-11 17:00:52 +0000

    1. removed config

--- hipchat/index.js
+++ hipchat/index.js
@@ -5,69 +5,97 @@ var HIPCHAT = require('./lib/hipchat');
 var hipchat = new HIPCHAT(config.hipchatAPIKey);
 
 
-module.exports = function(game) {
+module.exports = function (game) {
 
-  game.eventEmitter.on("roomNotification", function (message) {
-      hipchat.postRequest('room/'+config.roomId+'/message', {"message":message},function(err,response){
-          console.log("err:"+err);
-          console.log("response:"+response);
-      });
-  });
-
-  game.eventEmitter.on("privateMessage", function (message, id) {
-      hipchat.postRequest('user/'+id+'/message', {"message":message, "notify":true},function(err,response){
-          console.log("err:"+err);
-          console.log("response:"+response);
-      });
-  });
-  
-  var cmdMapper = [{
-    "cmd": "/start",
-    "method": function() {
-      return game.startGame();
-    }
-  },
-    {
-      "cmd":"/join",
-      "method":function(message){
-        return game.joinPlayer("@"+message.from.mention_name, message.from.id);
-      }
-    }
-  ];
-
-  var lastCheckTimestamp = Date.now();
-
-  var mainLoop = function() {
-    // this will list all of your rooms
-    hipchat.getRequest('room/'+config.roomId+'/history/latest', function(err, history){
-      if(history && history.items) {
-
-        history.items.forEach(function(message) {
-
-          if(new Date(message.date).getTime() > lastCheckTimestamp&& message.from.id!==config.adminId) {
-            lastCheckTimestamp = new Date(message.date).getTime();
-            console.log('New message: ' + message.message);
-            cmdMapper.forEach(function(cmd) {
-              if(message.message.search(cmd.cmd) !== -1) {
-                console.log('Found cmd: ' + cmd.cmd);
-                var e = cmd.method(message);
-                
-                hipchat.postRequest('room/'+config.roomId+'/message', {"message":e},function(err,response){
-                    console.log("err:"+err);
-                    console.log("response:"+response);
-                });
-                
-              }
-            });
-          }
+    game.eventEmitter.on("roomNotification", function (message, roomId) {
+        hipchat.postRequest('room/' + roomId + '/message', {"message": message}, function (err, response) {
+            console.log("roomNotification  err:" + err);
         });
-
-      } else {
-        
-        console.log(history)
-      }
     });
-  }
 
-  setInterval(mainLoop, 5000);
+    game.eventEmitter.on("privateMessage", function (message, id) {
+        hipchat.postRequest('user/' + id + '/message', {"message": message, "notify": true}, function (err, response) {
+            console.log("privateMessage err:" + err);
+        });
+    });
+
+    game.eventEmitter.on("deleteRoom", function (id) {
+        hipchat.deleteRequest('/room/' + id, null, function (err, response) {
+            console.log("deleteRoom err:" + err);
+        });
+    });
+
+    game.eventEmitter.on("listenToRoom", function (id) {
+        hipchat.deleteRequest('/room/' + id, null, function (err, response) {
+            console.log("deleteRoom err:" + err);
+        });
+    });
+
+    game.eventEmitter.on("createRoomAndInvite", function (name, topic, playerIds) {
+        hipchat.postRequest('/room', {"name": name, "privacy": "private", "topic": topic}, function (err, response) {
+
+            console.log("createRoom err:" + err);
+            game.wolfRoomId = response.id;
+            playerIds.forEach(function (id) {
+                hipchat.postRequest('/room/' + response.id + '/invite/' + id, {
+                    "name": name,
+                    "privacy": "private"
+                }, function (err, response) {
+                    console.log("invite err:" + err);
+                });
+            });
+
+        });
+    });
+
+    var cmdMapper = [
+        {
+            "cmd": "/start",
+            "method": function () {
+                return game.startGame();
+            }
+        },
+        {
+            "cmd": "/join",
+            "method": function (message) {
+                return game.joinPlayer("@" + message.from.mention_name, message.from.id);
+            }
+        }
+    ];
+
+    var lastCheckTimestamp = Date.now();
+
+    var mainLoop = function () {
+        // this will list all of your rooms
+        hipchat.getRequest('room/' + config.roomId + '/history/latest', function (err, history) {
+            if (history && history.items) {
+
+                history.items.forEach(function (message) {
+
+                    if (new Date(message.date).getTime() > lastCheckTimestamp && message.from.id !== config.adminId) {
+                        lastCheckTimestamp = new Date(message.date).getTime();
+                        console.log('New message: ' + message.message);
+                        cmdMapper.forEach(function (cmd) {
+                            if (message.message.search(cmd.cmd) !== -1) {
+                                console.log('Found cmd: ' + cmd.cmd);
+                                var e = cmd.method(message);
+
+                                hipchat.postRequest('room/' + config.roomId + '/message', {"message": e}, function (err, response) {
+                                    console.log("err:" + err);
+                                    console.log("response:" + response);
+                                });
+
+                            }
+                        });
+                    }
+                });
+
+            } else {
+
+                console.log(history)
+            }
+        });
+    }
+
+    setInterval(mainLoop, 5000);
 };
--- hipchat/lib/hipchat.js
+++ hipchat/lib/hipchat.js
@@ -1,37 +1,52 @@
 var request = require('request');
 
-var Hipchat = function(token, api_root) {
+var Hipchat = function (token, api_root) {
     this.token = token;
     this.api_root = api_root || 'https://api.hipchat.com/v2/';
 }
 
 
-Hipchat.prototype.getRequest = function(endpoint, callback) {
-  var self = this;
-  
-  request(self.api_root + endpoint + '?auth_token='+ self.token, function(err, res, body) {
-    if(!err && res.statusCode === 200) {
-      return callback(null, JSON.parse(body));
-    }
-    
-    return callback(err, JSON.parse(body));
-  });
+Hipchat.prototype.getRequest = function (endpoint, callback) {
+    var self = this;
+
+    request(self.api_root + endpoint + '?auth_token=' + self.token, function (err, res, body) {
+        if (!err && res.statusCode === 200) {
+            return callback(null, JSON.parse(body));
+        }
+
+        return callback(err, JSON.parse(body));
+    });
 }
 
-Hipchat.prototype.postRequest = function(endpoint, postObject, callback) {
-  var self = this;
-  console.log(postObject);
-  request({
-	  	url:self.api_root + endpoint + '?auth_token='+ self.token,
-	  	method:"POST",
-	  	json: postObject
-	}, function(err, res, body) {
-	    if(!err && res.statusCode === 200) {
-	      return callback(null, body);
-	    }
-	    
-	    return callback(err,body);
-  });
+Hipchat.prototype.postRequest = function (endpoint, postObject, callback) {
+    var self = this;
+    request({
+        url: self.api_root + endpoint + '?auth_token=' + self.token,
+        method: "POST",
+        json: postObject
+    }, function (err, res, body) {
+        if (!err && res.statusCode === 200) {
+            return callback(null, body);
+        }
+
+        return callback(err, body);
+    });
+}
+
+
+Hipchat.prototype.deleteRequest = function (endpoint, postObject, callback) {
+    var self = this;
+    request({
+        url: self.api_root + endpoint + '?auth_token=' + self.token,
+        method: "DELETE",
+        json: postObject
+    }, function (err, res, body) {
+        if (!err && res.statusCode === 200) {
+            return callback(null, body);
+        }
+
+        return callback(err, body);
+    });
 }
 
 
